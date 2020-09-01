d33de6feb374d073a004b766f7ecbd231f092d86
Zhihui Tang <crafttang@gmail.com>
2020-09-01 13:34:15 +0200

    opt

--- main.py
+++ main.py
@@ -5,32 +5,46 @@ origin_provsvar = 'Provsvar-fixed.xlsx'
 output_file = 'provsvar-summary.xlsx'
 config = pd.read_excel("config.xlsx")
 
+
+class LabItem:
+    def __init__(self, name, min, max):
+        self.name = name
+        self.min = min
+        self.max = max
+
 # Provsvar-fixed
 provsvar = pd.read_excel(origin_provsvar, sheet_name='Sheet1')
 
-# Create a Pandas Excel writer using XlsxWriter as the engine.
-writer = pd.ExcelWriter(output_file, engine='xlsxwriter', options={'strings_to_numbers': True})
-
-provsvar.to_excel(writer, sheet_name='Sheet1')
-# Get the xlsxwriter workbook and worksheet objects.
-workbook = writer.book
-worksheet = writer.sheets['Sheet1']
-# Add a format. Light red fill with dark red text.
-format1 = workbook.add_format({'bg_color': '#FFC7CE', 'font_color': '#9C0006'})
-
 # Apply a conditional format to the cell range.
 # worksheet.conditional_format(0, 0, 200, 200, {'type': 'cell', 'criteria': '>', 'value': 1.33, 'format': format1})
-headers = []
-for i in config.index:
-    item = config['Name'].at[i]
-    headers.append(item)
+
+
+bad_style = 'background-color: #FFC7CE'
+
+lab_items = []
+for i, row in config.iterrows():
+    lab_item = LabItem(row['Name'], row['Min'], row['Max'])
+    lab_items.append(lab_item)
+
+print(config)
 
 for row in provsvar.index:
-    for header in headers:
-        value = provsvar[header].at[row]
+    for lab_item in lab_items:
+        value = provsvar[lab_item.name].at[row]
         if isinstance(value, str):
             # Fix abnormal cell: "0,33, 0,33", convert "," to "."
-            provsvar[header].at[row] = float(value.split(", ")[0].replace(",", "."))
+            provsvar[lab_item.name].at[row] = float(value.split(", ")[0].replace(",", "."))
 
-provsvar.set_index('Datum', inplace=True)
-provsvar.to_excel(output_file, sheet_name='summary', columns=headers, )
+headers = list(map(lambda x: x.name, lab_items))
+print(f'headers: {headers}')
+provsvar_new = pd.DataFrame(provsvar, columns=headers)
+
+provsvar_new.style\
+    .applymap(lambda x: bad_style if x > 1.33 else '',
+              subset=['P--Calciumjon, fri (mmol/L)'])\
+    .to_excel(output_file, engine='openpyxl', sheet_name='summary', index=False)
+
+
+
+# provsvar_styled.set_index('Datum', inplace=True)
+# provsvar_styled.to_excel(output_file, engine='openpyxl', sheet_name='summary', columns=headers, index=False)
